fa4eb81534e4d0d12e43206c290c04b9a468d974
Misakakioo <2696352048@qq.com>
2025-10-05 17:15:08 +0800

    upload reports

--- read_checked_cal.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-# Read the CSV file
-df = pd.read_csv("100_read_checked.csv")
-
-# Count the number of 1s in the 'Flipped' column
-count = (df["Flipped"] == 1).sum()
-
-print(count)
